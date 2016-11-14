377e00a2112d6ecf6c36cc240169a7b2ab1ffdf0
QianyuCheng <qc510@nyu.edu>
2016-11-13 23:12:10 -0500

    commit test and interval

--- qc510/test.py
+++ qc510/test.py
@@ -1,16 +1,35 @@
-'''
-Created on Nov 13, 2016
-
-@author: Margaret
-'''
 import unittest
+from assignment7 import interval,mergeOverlapping,insert,mergeIntervals
 
+class UnitTestForInter(unittest.TestCase):
+    
+    def test_constructor(self):
+        class_test = interval('[2,6)')
+        class_interv = '[2,6)'
+        class_front='['
+        class_lower=2
+        class_upper=6
+        class_end = ')'
+        self.assertEqual(class_test.interv,class_interv)
+        self.assertEqual(class_test.front,class_front)
+        self.assertEqual(class_test.lower,class_lower)
+        self.assertEqual(class_test.upper,class_upper)
+        self.assertEqual(class_test.end,class_end)
+    def test_mergeIntervals(self):
+        class_test_first = mergeIntervals('[2,6]','[7,10)')
+        class_result_first = '[2,10)'
+        self.assertEqual(class_test_first,class_result_first)
+        
 
-class Test(unittest.TestCase):
-
-
-    def testName(self):
-        pass
+    def test_mergeOverlapping(self):
+        class_test_first = mergeOverlapping('[2,6],[7,10),[12,15)')
+        class_result_first = ['[2,10)','[12,15)']
+        self.assertEqual(class_test_first,class_result_first)
+    
+    def test_insert(self):
+        class_test_first = insert(['[2,6]','[7,10)','[12,15)'],'[17,19]')
+        class_result_first =['[2,10)', '[12,15)', '[17,19]']
+        self.assertEqual(class_test_first,class_result_first)
 
 
 if __name__ == "__main__":
