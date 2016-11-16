93d777272641546e265d4438e162efd9a2336680
yrmuriel820 <yrmuriel820@gmail.com>
2016-11-16 02:07:12 -0500

    notes

--- ym1495_assignment7/interval_assignment7/interval.py
+++ ym1495_assignment7/interval_assignment7/interval.py
@@ -18,18 +18,20 @@ class CommaError(IntervalErrors):
 class IntervalNoExistence(IntervalErrors):
     def __str__(self):
         return 'Your interval has a non positive size, please check\n'
-#class Unmergeable(Exception):
-#    def __str__(self):
-#        return 'Your interval has a non positive size, please check\n'
+
 
 class Interval(object):
+    
+    
     '''
-    classdocs
-    '''
-         
+    Define the constructor of object 'Interval'
+    '''         
     def __init__(self, interval_input):
+        
         '''
-        Constructor
+        Check whether input is a string input, if not raise error
+        Check whether input provides correct brackets, if not raise error
+        Check whether two integers are separated by comma, if not raise error
         '''
         if type(interval_input) != str:
             raise InputNotString
@@ -41,6 +43,19 @@ class Interval(object):
         self.inner = self.input[1:-1].split(",")
         if len(self.inner) != 2 :
             raise CommaError()
+
+        
+        
+        '''
+        define inclusive or exclusive with brackets, set default as [a,b)
+        because default of range in python is also lower inclusive, upper exclusive.
+        store corresponding lowerbound and upperbound by default form
+        store the list of integers included in such an interval
+        raise exception if there's paradox in input
+        (such as: lower bound excluded a number, while upper bound included it
+        or lower bound is greater than upper bound)
+        it will also raise ValueError by default if input are not integers(but float, double, etc.)
+        '''
         self.left_end = int(self.input[1:-1].split(",")[0])
         self.right_end = int(self.input[1:-1].split(",")[1])
         if self.left_bracket == '(':
@@ -53,16 +68,27 @@ class Interval(object):
         for x in range(self.left_end,self.right_end):
             self.integerlist.append(x)
     
+    '''
+    Define output form of interval
+    '''
     def __repr__(self):
         str =  '%s ' % self.input
         return str
     
+    '''
+    Define equality between two intervals by comparing their uniformed lower and upper bounds
+    '''
     def __eq__(self, other):
         return (self.left_end == other.left_end and self.right_end == other.right_end)
-    
+
+
+'''
+Define function to merge two intervals, 
+return to a tuple of input intervals if unable to merge
+return to a new interval if merge succeeded
+'''    
 def mergeIntervals(int1,int2):
     if (int1.left_end >int2.right_end or int2.left_end >int1.right_end):
-        print('Unable to merge')
         return int1,int2
     else:
         left_end = min(int1.left_end, int2.left_end)
@@ -71,23 +97,29 @@ def mergeIntervals(int1,int2):
         new_merge = Interval(str)
         return new_merge
 
+'''
+Sort the interval list first by their lowerbound, because of reason below:
+For example, a sorted interval list a has a[0],a[1],a[2]
+if a[0] cant merge with a[1], it means a[0].right_end < a[1].left_end <a[2].left_end
+which means a[0] cant merge with a[2] either.
+'''
 def mergeOverlapping(intervals):
     if len(intervals) in range(0, 1):
         return intervals
     else:
         intervals = sorted(intervals, key = lambda interval: interval.left_end)
-#        print('Sort the list by lower bound)
         merged_list = [intervals[0]]
         for i in range(0, len(intervals)):
             new_merge = mergeIntervals(merged_list[-1], intervals[i])
             if type(new_merge) == tuple:
                 merged_list.append(intervals[i])
-#                print(' No Merge once')
             else:
                 merged_list[-1] = new_merge
-#                print('merge once' )
         return merged_list
-    
+
+'''
+insert a new interval into an interval list by append,sort,and mergeOverlapping
+'''    
 def insert(intervals, newint):
     if type(newint) != Interval:
         newint = Interval(newint)
--- ym1495_assignment7/interval_assignment7/ym1495.py
+++ ym1495_assignment7/interval_assignment7/ym1495.py
@@ -7,36 +7,45 @@ import sys
 from interval_assignment7.interval import *
 
 def main():
-    user_input = []    
-    user_input = input("Please enter a list of intervals separated by ',':")
-    if user_input == "quit":
-        pass
-            
-    else:
-        half_interval = user_input.split(',')
-#        if len(half_interval)/2 !=0:
-#            raise IntervalErrors('Please check your input ')
-        intervals = []
-        for i in range(0, len(half_interval),2):
-            newInt = Interval('%s,%s'%(half_interval[i],half_interval[i+1]))
-            insert(intervals, newInt)
+    '''
+    As integers and intervals are both separated by comma,
+    I split them into pieces and recombine together to make intervals
+    if exceptions occur, it will ask for a new input
+    and break when get one interval list correctly
+    '''
+    while True:
+        try:
+            user_input = []    
+            user_input = input("Please enter a list of intervals separated by ',':")
+            if user_input == "quit":
+                sys.exit(0)
+            else:
+                half_interval = user_input.split(',')
+                intervals = []
+                for i in range(0, len(half_interval),2):
+                    newInt = Interval('%s,%s'%(half_interval[i],half_interval[i+1]))
+                    insert(intervals, newInt)
+                print(intervals)
+                break
+        except InputNotString and MissingBracket and CommaError and IntervalNoExistence and ValueError:
+            print("Invalid input, please try again")
     
+    '''
+    keep asking for new interval input until told to quit
+    '''
     while True:
         try:
             user_input_int = input("Interval? ")
             if "quit" in user_input_int:
-                pass
+                sys.exit(0)
             new_int = Interval(user_input_int)
             new_intervals = insert(intervals, new_int)
             print (new_intervals)
  
-        except IntervalErrors:
+        except InputNotString and MissingBracket and CommaError and IntervalNoExistence and ValueError:
             print("Invalid interval, please try again")
             continue
+
         
-            
-        
-#    except ValueError:
- #       print('/')
 if __name__ == '__main__':
     main()
